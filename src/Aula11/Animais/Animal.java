eb73357813bd7e71ccccb9af544df80954a1415c
alexandrecpedro <alexandrecpedro@gmail.com>
2021-11-16 10:05:56 -0300

    Abstract Classes

--- src/Aula11/Animais/Animal.java
+++ src/Aula11/Animais/Animal.java
@@ -12,6 +12,23 @@ public abstract class Animal {
         this.idade = idade;
     }
 
-//    Methods
+//    Getters/Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    //    Methods
     public abstract void emitirSom();
 }
