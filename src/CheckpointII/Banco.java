182e54595aabb8c5ad78998263760509b5750963
alexandrecpedro <alexandrecpedro@gmail.com>
2021-11-22 17:08:02 -0300

    Checkpoint II - Building Model with Java

---
+++ src/CheckpointII/Banco.java
@@ -0,0 +1,17 @@
+package CheckpointII;
+
+public class Banco {
+//    Attributes
+    private String nome;
+    private String agencia;
+    private String endereco;
+    private String telefone;
+
+//    Constructor
+    public Banco(String nome, String agencia, String endereco, String telefone) {
+        this.nome = nome;
+        this.agencia = agencia;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+}
