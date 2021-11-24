9a3c16208e94c0858e768599f084665effb4b31d
alexandrecpedro <alexandrecpedro@gmail.com>
2021-11-24 10:38:54 -0300

    Review - Collections with JAVA

---
+++ src/Aula15/Veleiro.java
@@ -0,0 +1,18 @@
+package Aula15;
+
+public class Veleiro extends Embarcacao {
+//    Attributes
+    private int numMastros;
+
+//    Constructor
+    public Veleiro(double precoBase, double valorAdicional, int anoFabricacao, double comprimento, Capitao capitao, int numMastros) {
+        super(precoBase, valorAdicional, anoFabricacao, comprimento, capitao);
+        this.numMastros = numMastros;
+    }
+
+//    Getters/Setters
+//    Methods
+    public void avaliarTamanho(){
+        System.out.println(this.numMastros > 4 ? "Veleiro grande": "Veleiro pequeno");
+    }
+}
